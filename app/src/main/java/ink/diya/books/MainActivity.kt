6643dac73e9d2f3201c9b8e9c4b43035042f7378
Jp-anix <jamespalmerm0701@gmail.com>
2023-04-23 17:29:56 +0100

    Added WebView to load diya.ink Added WebView.goBack()
    Updated gradle to latest version for App dir

--- app/src/main/java/ink/diya/books/MainActivity.kt
+++ app/src/main/java/ink/diya/books/MainActivity.kt
@@ -1,11 +1,46 @@
 package ink.diya.books
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.KeyEvent
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.activity.ComponentActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+
+    private var webView : WebView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        webView = WebView(this)
+        setContentView(webView)
+        webView!!.loadUrl("https://diya.ink/")
+        webView!!.settings.javaScriptEnabled = true
+        webView!!.addJavascriptInterface(WebAppInterface(this), "Android")
+        webView!!.webViewClient = WebViewClient()
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        // Check if the key event was the Back button and if there's history
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView!!.canGoBack()) {
+            webView!!.goBack()
+            return true
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event)
+    }
+}
+
+class WebAppInterface(private val mContext: Context) {
+    /** Show a toast from the web page  */
+    @JavascriptInterface
+    fun showToast(toast: String) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show()
     }
 }
