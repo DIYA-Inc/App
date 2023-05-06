3c8d5607236468dbe6234e381f8e2fbdf0b8bb28
Joe Baker <joeblakeb@gmail.com>
2023-05-06 16:29:17 +0100

    Pull down to refresh page

--- app/src/main/java/ink/diya/books/MainActivity.kt
+++ app/src/main/java/ink/diya/books/MainActivity.kt
@@ -1,44 +1,54 @@
 package ink.diya.books
 
+import android.annotation.SuppressLint
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.KeyEvent
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : ComponentActivity() {
+    private lateinit var swipeRefreshLayout : SwipeRefreshLayout
+    private lateinit var webView : WebView
 
-    private var webView : WebView? = null
-
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        webView = WebView(this)
-        setContentView(webView)
-        webView!!.loadUrl("https://diya.ink/")
-        webView!!.settings.javaScriptEnabled = true
-        webView!!.addJavascriptInterface(WebAppInterface(this), "Android")
-        webView!!.webViewClient = WebViewClient()
+        setContentView(R.layout.activity_webview)
+
+        webView = findViewById(R.id.webView)
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout)
+
+        webView.loadUrl("https://diya.ink/")
+        webView.settings.javaScriptEnabled = true
+        webView.addJavascriptInterface(WebAppInterface(this), "Android")
+        webView.webViewClient = WebViewClient()
+
+        swipeRefreshLayout.setOnRefreshListener {
+            webView.reload()
+            swipeRefreshLayout.isRefreshing = false
+        }
     }
 
+    /**
+     * Go back on the WebView if there is history,
+     * if not, bubble up to systems behavior (exit the activity)
+     */
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        // Check if the key event was the Back button and if there's history
-        if (keyCode == KeyEvent.KEYCODE_BACK && webView!!.canGoBack()) {
-            webView!!.goBack()
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
             return true
         }
-        // If it wasn't the Back key or there's no web page history, bubble up to the default
-        // system behavior (probably exit the activity)
         return super.onKeyDown(keyCode, event)
     }
 }
 
 class WebAppInterface(private val mContext: Context) {
-    /** Show a toast from the web page  */
+    /** Show a toast from the web page */
     @JavascriptInterface
     fun showToast(toast: String) {
         Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show()
